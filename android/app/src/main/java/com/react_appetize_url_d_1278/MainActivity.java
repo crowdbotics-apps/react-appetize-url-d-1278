eef021052f161858ac2d5c92cf8b523d037e2da5
Crowdbotics <team@crowdbotics.com>
2019-12-23 23:14:26 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/react_appetize_url_d_1278/MainActivity.java
@@ -0,0 +1,15 @@
+package com.react_appetize_url_d_1278;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "react_appetize_url_d_1278";
+    }
+}
